8205430f457858ed3bd6f5a2970b7f03e95430d3
Your Name <you@example.com>
2019-10-22 16:58:02 +0800

    init app

---
+++ build/precache-manifest.aafd582c64b89029cc2d0efb1fc7b072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aca6a6d6ca09887cf67789cd4eb3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d0bb5705885b627f3e5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0375f00f7206ea7d1301",
+    "url": "/static/js/2.c6ba6379.chunk.js"
+  },
+  {
+    "revision": "8d0bb5705885b627f3e5",
+    "url": "/static/js/main.089f60a4.chunk.js"
+  },
+  {
+    "revision": "a97e715d3a59754ea336",
+    "url": "/static/js/runtime-main.45f48665.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
